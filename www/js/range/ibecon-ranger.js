73939cdaaa918939a633d77a5ee51885375ab7a0
Franco Borrelli <francoborrelli96@outlook.com>
2018-06-20 23:50:55 -0300

    Add distance to range info

--- www/js/range/ibecon-ranger.js
+++ www/js/range/ibecon-ranger.js
@@ -15,11 +15,31 @@ class IbeaconRanger extends Range {
       beacon.tx +
       "<br> Proximity: " +
       beacon.proximity +
-      "<br> rssi: " +
+      "<br> Distance: " +
+      this.calculateDistance(beacon) +
+      " meters <br> rssi: " +
       beacon.rssi +
       "<br> Accuracy: " +
       beacon.accuracy +
       "<hr>"
     );
+  }
+
+  calculateDistance(beacon) {
+    if (beacon.rssi == 0) {
+      return -1.0;
+    }
+    var ratio = (beacon.rssi * 1.0) / beacon.tx;
+    if (ratio < 1.0) {
+      var distance = Math.pow(ratio, 10);
+    } else {
+      var distance = 0.89976 * Math.pow(ratio, 7.7095) + 0.111;
+    }
+    return this.roundNumber(distance);
+  }
+
+  roundNumber(number) {
+    const decimals = 100000;
+    return Math.round(number * decimals) / decimals;
   }
 }
